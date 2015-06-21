e68c4b989a462e3ec7a32e2624eb7e175a26244e
Alexander Zielenski <alex@alexzielenski.com>
2015-06-21 17:11:54 -0400

    fix color component order

--- ThemeKit/ThemeKit/Headers/TKStructs.h
+++ ThemeKit/ThemeKit/Headers/TKStructs.h
@@ -95,10 +95,11 @@ typedef struct {
 
 #pragma mark - Colors
 
+// I thin it was RGBA in the old car files?
 struct rgbquad {
-    uint8_t r;
-    uint8_t g;
     uint8_t b;
+    uint8_t g;
+    uint8_t r;
     uint8_t a;
 };
 
