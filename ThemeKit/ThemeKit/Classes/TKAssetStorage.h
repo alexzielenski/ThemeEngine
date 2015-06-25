c81b84b24ea2812176f0359374c8d1a1cda94ad6
Alexander Zielenski <alex@alexzielenski.com>
2015-06-25 11:57:06 -0400

    little bit of docs

--- ThemeKit/ThemeKit/Classes/TKAssetStorage.h
+++ ThemeKit/ThemeKit/Classes/TKAssetStorage.h
@@ -10,17 +10,61 @@
 #import <ThemeKit/TKElement.h>
 #import <ThemeKit/TKTypes.h>
 
-// ThemeKit is for editing files ONLY, you may not create a new file with this
+/**
+ *  TKAssetStorage represents a car file on disk. It populates its elements and renditions lists
+ * based on the contents of the car file. You can use this class to obtain a list of all elements or
+ * renditions available in the car file. If you wish to save your car file, refer to TKMutableAssetStorage
+ */
 @interface TKAssetStorage : NSObject
+/**
+ *  Set of TKElements belonging to this asset storage
+ *  @see TKElement
+ */
 @property (readonly, strong) NSSet<TKElement *> *elements;
+/**
+ *  Undo manager to which we register the undo actions.
+ *  @warning Settings this property to nil will still have the performance hit
+ *  of registering undo properties. Create a category on all subclasses of TKModelObject
+ *  and override `-(BOOL)wantsUndoHandling`
+ *  @warning This property must be non-nil for `dirty` to be marked.
+ *  @see TKModelObject
+ */
 @property (strong) NSUndoManager *undoManager;
+/**
+ *  Path to the asset storage
+ *  @warning This path is not reliable. CoreUI's backing layout often creates
+ *  temporary file storage for asset catalogs
+ */
 @property (readonly, copy) NSString *path;
+/**
+ *  Indicator of if any renditions in this storage have had changes registered to them.
+ *  @warning This property will not be useful if the rendition's `-wantsUndoHandling` returns NO.
+ */
 @property (assign, getter=isDirty) BOOL dirty;
 
+/**
+ *  Creates an asset storage object
+ *
+ *  @param path Path to your .car file
+ *
+ *  @return A new asset storage object for managing the car file
+ */
 + (instancetype)assetStorageWithPath:(NSString *)path;
 - (instancetype)initWithPath:(NSString *)path;
 
+/**
+ *  @param name Name of the element you want to find.
+ *
+ *  @return an element with `name`, or NULL, if there is none
+ */
 - (TKElement *)elementWithName:(NSString *)name;
+
+/**
+ *  Gives a list of all renditions for all elements in this asset storage
+ *
+ *  @return an NSSet of TKRendition
+ *  @see TKRendition
+ */
 - (NSSet<TKRendition *> *)allRenditions;
 
 @end
--- ThemeKit/ThemeKit/Classes/TKMutableAssetStorage.h
+++ ThemeKit/ThemeKit/Classes/TKMutableAssetStorage.h
@@ -9,6 +9,21 @@
 #import <ThemeKit/TKAssetStorage.h>
 #import <ThemeKit/TKTypes.h>
 
+/**
+ *  Mutable version of TKAssetStorage that allows you to save changes made to renditions
+ */
 @interface TKMutableAssetStorage : TKAssetStorage
+/**
+ *  Commits all changes to renditions to disk
+ *
+ *  @param update Set this to YES if you wish for all renditions to be marked as non-dirty, and synced
+ * to disk. An example of when you would want this to be NO is in a "Save As" function where the document
+ * is saved to a new location but still represents the old one.
+ *
+ *  @warning Documents will not be committed if their change counts have not changed since the last save.
+ * either update their change counts manually, or make sure -wantsUndoHandling returns true
+ *
+ *  @see TKModelObject
+ */
 - (void)writeToDiskUpdatingChangeCounts:(BOOL)update;
 @end
